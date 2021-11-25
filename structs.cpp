20466f46e488f9cb537f8bc4f5bf3f1441b57884
Atharva Meshram <atharva10798@gmail.com>
2021-11-25 23:40:15 +0530

    Parser added

--- structs.cpp
+++ structs.cpp
@@ -45,10 +45,56 @@ int Tokenizer::errorCheck(){
     return 0;
 }
 
-void Parser::setString(std::string input){
-    sqlQuery = input;
-};
-
 void Parser::parse(){
+    for(int i=0; i<sqlCommands.size(); i++){
+        sqlQuery = sqlCommands[i];
+        
+        vector<string> temp;
+        stringstream ss(sqlQuery);
+        string word;
+        while (ss >> word) {
+            transform(word.begin(), word.end(), word.begin(), ::tolower);
+            temp.push_back(word);
+        }
 
+        if(temp[0] == "create" && temp[1] == "table"){
+            vector<string> colNames, colTypes;
+
+            for(int i=3; i<temp.size(); i++){
+                if(i==3){
+                    colNames.push_back(temp[i].substr(1));
+                }
+                else if(i == temp.size() - 1 || i % 2 == 0){
+                    colTypes.push_back(temp[i].substr(0, temp[i].size()-1));
+                }
+                else{
+                    colNames.push_back(temp[i]);
+                }
+            }
+
+            createDB(temp[2], colNames, colTypes);
+        }
+        else if(temp[0] == "drop"){
+            cout << "drop" << endl;
+        }
+        else
+            cout << "Error: " << errors[3] << " \"" << temp[0] << " " << temp[1] << "\"" << endl;
+    }
+}
+
+void createDB(std::string tableName, std::vector<std::string> colNames, std::vector<std::string> colTypes){
+    file.open("db/schema.txt", ios::app);
+    ofstream csvFile(tableName + " .csv");
+
+    file << tableName << "#";
+    csvFile << tableName << ",";
+    for(int i=0; i<colNames.size(); i++){
+        file << colNames[i] << "#" << colTypes[i];                  //Handle Char(12) len and stuff
+        csvFile << colNames[i] << "," << colTypes[i];
+    }
+    file << endl;
+    csvFile << endl;
+
+    file.close();
+    csvFile.close();
 }
--- structs.h
+++ structs.h
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
+#include <sstream>
 
 std::fstream file;                                  //Global file handler for I/O
 std::vector<std::string> sqlCommands;               //Set of SQL Commands provided by user
@@ -23,11 +25,8 @@ private:
     std::string sqlQuery;
 
 public:
-    void setString(std::string);
     void parse();
 };
-
-void execute();                                     //Execute takes Command name ("CREATE", "SELECT"), and call necessary functions to execute the commands
 
 /*DDL*/
 void createDB(std::string tableName, std::vector<std::string> colNames, std::vector<std::string> colTypes);
@@ -42,7 +41,7 @@ void deleteRow(std::string tableName, std::vector<std::string> colNames, std::st
 void select(std::string tableName, std::vector<std::string> colNames);
 
 /*Errors*/
-std::string errors[3] = {"No Error", "Missing right parenthesis!", "Missing left parenthesis!"};
+std::string errors[4] = {"No Error", "Missing right parenthesis!", "Missing left parenthesis!", "No such SQL Command!"};
 
 #include "structs.cpp"
 #endif
